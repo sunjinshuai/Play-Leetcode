7adacff7a25bf1e27eed0eff9c420039834b99bf
sunjinshuai <s_mike@163.com>
2018-09-01 17:28:30 +0800

    c two sum

--- 0001-Two-Sum/0001-Two-Sum/0001-C/main.c
+++ 0001-Two-Sum/0001-Two-Sum/0001-C/main.c
@@ -2,32 +2,22 @@
 #include <stdlib.h>
 
 int *twoSum(int *nums, int numsSize, int target) {
-    
-    int index1 = 0,index2 = 0;
-    for (int i = 0; i < numsSize; i++) {
-        for (int j = 0; j < numsSize; j++) {
+    int *array = (int*)malloc(2*sizeof(int));
+      for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; (j < numsSize && j != i); j++) {
             if (nums[i] + nums[j] == target) {
-                if (nums[i] <= nums[j]) {
-                    index1=i+1;
-                    index2=j+1;
-                } else {
-                    index1=j+1;
-                    index2=i+1;
-                }
+                array[0] = i;
+                array[1] = j;
             }
         }
     }
-    
-    printf("[%d,%d]",index1,index2);
-    int *array = (int *)malloc(sizeof(int) * 2);
-    array[0] = index1;
-    array[1] = index2;
     return array;
 }
 
 int main(int argc, const char * argv[]) {
     int nums[] = {2, 7, 11, 15, 1, 8};
     int *p = twoSum(nums, 6, 9);
+    printf("[%d,%d]",p[0],p[1]);
     free(p);
     return 0;
 }
