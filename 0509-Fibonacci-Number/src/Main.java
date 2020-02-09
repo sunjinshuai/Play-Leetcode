bc7a8822f7ad9418c1c9b7d25649a2747e9bccc9
sunjinshuai <s_mike@163.com>
2020-02-09 19:46:33 +0800

    0509-Fibonacci-Number

---
+++ 0509-Fibonacci-Number/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    // 时间复杂度 O(N*N)
+    public static int fib1(int N) {
+        if (N <= 1) return N;
+        return fib1(N - 1) + fib1(N - 2);
+    }
+
+    // 时间复杂度 O(N)
+    public static int fib2(int N) {
+        int firstNum = 0;
+        int secondNum = 1;
+        for (int i = 0; i < N; i++) {
+            secondNum += firstNum;
+            firstNum = secondNum - firstNum;
+        }
+        return secondNum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib2(2));
+        System.out.println(fib2(3));
+        System.out.println(fib2(4));
+        System.out.println(fib2(5));
+    }
+}
