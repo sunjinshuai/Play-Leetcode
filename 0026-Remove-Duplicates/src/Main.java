49a230bfcf85385b1e79390a622ff970b4152142
sunjinshuai <s_mike@163.com>
2020-02-16 20:28:50 +0800

    0026-Remove-Duplicates

---
+++ 0026-Remove-Duplicates/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i + 1]) {
+                nums[j] = nums[i + 1];
+                j++;
+            }
+        }
+        return j;
+    }
+}
