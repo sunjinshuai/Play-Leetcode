2b9aaf2e826358979e39cea491d8722aef5cf5ee
sunjinshuai <s_mike@163.com>
2020-04-15 17:15:26 +0800

    0060-Plus-One

--- 0066-Plus-One/Java/Main.java
+++ 0066-Plus-One/Java/Main.java
@@ -8,8 +8,7 @@ public class Main {
         for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] == 9) {
                 digits[i] = 0;
-            }
-            if (digits[i] != 0) {
+            } else {
                 digits[i]++;
                 return digits;
             }
